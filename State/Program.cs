6fe9ed011b2656fee48515d3375b8e58ba3793c2
rda83 <rda83@mail.ru>
2019-07-06 14:13:03 +0700

    +State

--- State/Program.cs
+++ State/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * GOF #20
+ * State (Состояние - паттерн поведения объектов.)
+ * Позволяет объекту варьировать свое поведение в зависимости от внутреннего состояния. 
+ * Извне создается впечатление, что изменился класс объекта.
+ * 06/07/2019
+ */
+
+using System;
 
 namespace State
 {
@@ -10,6 +14,59 @@ namespace State
     {
         static void Main(string[] args)
         {
+            Context context = new Context();
+            context.Request();
+            context.Request();
+            Console.ReadLine();
+        }
+    }
+
+    public class Context
+    {
+        State _state;
+
+        public Context()
+        {
+            this._state = new State_0();
+        }
+
+        public void Request()
+        {
+            _state.Handle(this);
+        }
+
+        internal void ChangeState(State state)
+        {
+            this._state = state;
+        }
+    }
+
+    public abstract class State
+    {
+        public abstract void Handle(Context context);
+
+        protected virtual void ChangeState(Context context, State state)
+        {
+            context.ChangeState(state);
+        }
+    }
+
+    public class State_0 : State
+    {
+        public override void Handle(Context context)
+        {
+            Console.WriteLine("State_0 - > State_1");
+            ChangeState(context, new State_1());
+        }
+    }
+
+    public class State_1 : State
+    {
+        public override void Handle(Context context)
+        {
+            Console.WriteLine("State_1 - > State_0");
+            ChangeState(context, new State_0());
         }
     }
 }
+    
