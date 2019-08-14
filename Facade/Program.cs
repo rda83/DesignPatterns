0419cb6fc9d0ffdb2b0f726c4f822ed0cb3d8894
rda83 <rda83@mail.ru>
2019-08-14 12:35:14 +0700

    +Facade

---
+++ Facade/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * GOF #10
+ * Facade (Фасад - паттерн, структурирующий объекты.)
+ * Предоставляет унифицированный интерфейс вместо набора интерфейсов ненкоторой подсистемы.
+ * Фасад определяет интерфейс более высокого уровня, который упрощает использование подсистемы.
+ * 14/08/2019
+ */
+
+using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.OperationAB();
+            facade.OperationBC();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Facade
+    {
+        SubSystemA subSystemA = new SubSystemA();
+        SubSystemB subSystemB = new SubSystemB();
+        SubSystemC subSystemC = new SubSystemC();
+
+        public void OperationAB()
+        {
+            subSystemA.OperationA();
+            subSystemB.OperationB();
+        }
+
+        public void OperationBC()
+        {
+            subSystemB.OperationB();
+            subSystemC.OperationC();
+        }
+    }
+
+    class SubSystemA
+    {
+        public void OperationA()
+        {
+            Console.WriteLine("OperationA");
+        }
+    }
+
+    class SubSystemB
+    {
+        public void OperationB()
+        {
+            Console.WriteLine("OperationB");
+        }
+    }
+
+    class SubSystemC
+    {
+        public void OperationC()
+        {
+            Console.WriteLine("OperationC");
+        }
+    }
+}
